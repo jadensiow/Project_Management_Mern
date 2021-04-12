9ec83f24024d1ea83848c9ca96dbd10c8f98a86c
jadensiow <jianhong93@gmail.com>
2021-04-13 04:24:49 +0800

    Start up

---
+++ server/server.js
@@ -0,0 +1,38 @@
+const express = require("express");
+const mongoose = require("mongoose");
+require("dotenv").config();
+
+const app = express();
+
+// Middleware
+app.use(express.json({ extended: false }));
+app.use(express.urlencoded({ extended: false }));
+
+// Connect database
+(async function connectDB() {
+  try {
+    await mongoose.connect(process.env.MONGO_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      useCreateIndex: true, // set default index to  createIndex as to avoid depreciation warnings
+      useFindAndModify: false, // Set to false to make findOneAndUpdate() and findOneAndRemove() use native findOneAndUpdate() rather than findAndModify().
+    });
+    console.log("Connect Mongo Database");
+  } catch (err) {
+    console.log(`Mongo server not connected`);
+    console.log(err);
+  }
+})();
+
+// Import Routes
+const usersController = require("./routes/users");
+const authController = require("./routes/auth");
+const boardController = require("./routes/board");
+// Routes
+app.use("/api/users", usersController);
+app.use("/api/auth", authController);
+app.use("/api/board", boardController);
+
+const PORT = process.env.PORT || 5001;
+
+app.listen(PORT, () => console.log("Server running on port:" + PORT));
