b248b303b4b3f909b8f4e1a8f84d02cfc60d9e4e
shalahuddin-ali <m.shalahuddin.ali@gmail.com>
2021-04-20 11:11:00 +0800

    minor editing

--- client/src/components/ganttChart/GanttData.js
+++ client/src/components/ganttChart/GanttData.js
@@ -1,224 +1,30 @@
-const dataSource = {
-  chart: {
-    dateformat: "mm/dd/yyyy",
-    caption: "Event Planning Process",
-    theme: "Candy",
-    canvasborderalpha: "80",
-    ganttlinealpha: "100",
-  },
-  tasks: {
-    color: "#5D62B5",
-    task: [
-      {
-        start: "03/07/2018",
-        end: "03/17/2018",
-      },
-      {
-        start: "03/14/2018",
-        end: "03/28/2018",
-      },
-      {
-        start: "03/15/2018",
-        end: "03/31/2018",
-      },
-      {
-        start: "04/02/2018",
-        end: "04/12/2018",
-      },
-      {
-        start: "04/12/2018",
-        end: "04/30/2018",
-      },
-      {
-        start: "04/20/2018",
-        end: "05/06/2018",
-      },
-      {
-        start: "04/30/2018",
-        end: "05/10/2018",
-      },
-      {
-        start: "04/30/2018",
-        end: "05/25/2018",
-      },
-      {
-        start: "05/04/2019",
-        end: "06/05/2019",
-      },
-      {
-        start: "04/20/2018",
-        end: "05/06/2018",
-      },
-      {
-        start: "04/30/2018",
-        end: "05/10/2018",
-      },
-      {
-        start: "04/30/2018",
-        end: "05/25/2018",
-      },
-      {
-        start: "05/04/2019",
-        end: "06/05/2019",
-      },
-    ],
-  },
-  processes: {
-    headertext: "Task",
-    headeralign: "left",
-    fontsize: "14",
-    isbold: "0",
-    align: "left",
-    process: [
-      {
-        label: "Define event goals",
-      },
-      {
-        label: "Source venue options",
-      },
-      {
-        label: "Finalize speaker reach out list",
-      },
-      {
-        label: "Compose sponsorship strategy",
-      },
-      {
-        label: "Reach out to sponsors",
-      },
-      {
-        label: "Create social media campaign",
-      },
-      {
-        label: "Reach out to blogs for backlinks",
-      },
-      {
-        label: "Optimize SEO ranking",
-      },
-      {
-        label: "Publish event lead up vlog series",
-      },
-      {
-        label: "Reach out to sponsors",
-      },
-      {
-        label: "Create social media campaign",
-      },
-      {
-        label: "Reach out to blogs for backlinks",
-      },
-      {
-        label: "Optimize SEO ranking",
-      },
-      {
-        label: "Publish event lead up vlog series",
-      },
-    ],
-  },
-  categories: [
-    {
-      category: [
-        {
-          start: "03/05/2018",
-          end: "03/31/2018",
-          label: "March",
-        },
-        {
-          start: "04/01/2018",
-          end: "04/30/2018",
-          label: "April",
-        },
-        {
-          start: "05/01/2018",
-          end: "05/31/2018",
-          label: "May",
-        },
-        {
-          start: "06/01/2018",
-          end: "06/10/2018",
-          label: "June",
-        },
-        {
-          start: "07/01/2018",
-          end: "07/10/2018",
-          label: "july",
-        },
-      ],
-    },
-    {
-      category: [
-        {
-          start: "03/05/2018",
-          end: "03/11/2018",
-        },
-        {
-          start: "03/12/2018",
-          end: "03/18/2018",
-        },
-        {
-          start: "03/19/2018",
-          end: "03/25/2018",
-        },
-        {
-          start: "03/26/2018",
-          end: "04/01/2018",
-        },
-        {
-          start: "04/02/2018",
-          end: "04/08/2018",
-          label: "W 5",
-        },
-        {
-          start: "04/09/2018",
-          end: "04/15/2018",
-          label: "W 6",
-        },
-        {
-          start: "04/16/2018",
-          end: "04/22/2018",
-          label: "W 7",
-        },
-        {
-          start: "04/23/2018",
-          end: "04/29/2018",
-          label: "W 8",
-        },
-        {
-          start: "04/30/2018",
-          end: "05/06/2018",
-          label: "W 9",
-        },
-        {
-          start: "05/07/2018",
-          end: "05/13/2018",
-          label: "W 10",
-        },
-        {
-          start: "05/14/2018",
-          end: "05/20/2018",
-          label: "W 11",
-        },
-        {
-          start: "05/21/2018",
-          end: "05/27/2018",
-          label: "W 12",
-        },
-        {
-          start: "05/28/2018",
-          end: "06/03/2018",
-          label: "W 13",
-        },
-        {
-          start: "06/04/2018",
-          end: "06/10/2018",
-          label: "W 14",
-        },
-        {
-          start: "06/04/2018",
-          end: "06/12/2018",
-          label: "W 14",
-        },
-      ],
-    },
-  ],
+const testData = {
+	chart: {
+		dateformat: "yyyy/mm/dd",
+		caption: "Event Planning Process",
+		theme: "Candy",
+		canvasborderalpha: "80",
+		ganttlinealpha: "100",
+	},
+	tasks: {
+		color: "#5D62B5",
+		task: [],
+	},
+	processes: {
+		headertext: "Task",
+		headeralign: "left",
+		fontsize: "14",
+		isbold: "0",
+		align: "left",
+		process: [],
+	},
+	categories: [
+		{
+			category: [],
+		},
+		{
+			category: [],
+		},
+	],
 };
-export default dataSource;
+export default GantDataTemplate;
--- client/src/components/pages/GanttChart.js
+++ client/src/components/pages/GanttChart.js
@@ -14,194 +14,169 @@ const moment = require("moment");
 ReactFC.fcRoot(FusionCharts, Gantt, FusionTheme);
 
 const GanttChart = ({ match }) => {
-  const [selectChart, setSelectChart] = useState("");
-  const board = useSelector((state) => state.board.board);
-  const isAuthenticated = useSelector((state) => state.auth.isAuthenticated);
-  const dispatch = useDispatch();
+	const [selectChart, setSelectChart] = useState("");
+	const board = useSelector((state) => state.board.board);
+	const isAuthenticated = useSelector((state) => state.auth.isAuthenticated);
+	const dispatch = useDispatch();
 
-  console.log(board);
-  console.log(board.cardObjects);
-  // if (!isAuthenticated) {
-  // 	return <Redirect to="/" />;
-  // }
+	console.log(board);
+	console.log(board.cardObjects);
+	// if (!isAuthenticated) {
+	// 	return <Redirect to="/" />;
+	// }
 
-  const testData = {
-    chart: {
-      dateformat: "yyyy/mm/dd",
-      caption: "Event Planning Process",
-      theme: "Candy",
-      canvasborderalpha: "80",
-      ganttlinealpha: "100",
-    },
-    tasks: {
-      color: "#5D62B5",
-      task: [],
-    },
-    processes: {
-      headertext: "Task",
-      headeralign: "left",
-      fontsize: "14",
-      isbold: "0",
-      align: "left",
-      process: [],
-    },
-    categories: [
-      {
-        category: [],
-      },
-      {
-        category: [],
-      },
-    ],
-  };
+	let oldDate = new Date(
+		Math.max.apply(
+			null,
+			board.cardObjects.map(function (e) {
+				return new Date(e.date.endDate);
+			})
+		)
+	);
 
-  let oldDate = new Date(
-    Math.max.apply(
-      null,
-      board.cardObjects.map(function (e) {
-        return new Date(e.date.endDate);
-      })
-    )
-  );
-  let newDate = new Date(
-    Math.min.apply(
-      null,
-      board.cardObjects.map(function (e) {
-        return new Date(e.date.startDate);
-      })
-    )
-  );
+	console.log("old", oldDate);
+	let newDate = new Date(
+		Math.min.apply(
+			null,
+			board.cardObjects.map(function (e) {
+				return new Date(e.date.startDate);
+			})
+		)
+	);
 
-  let dateToYMD = (date) => {
-    let d = date.getDate();
-    let m = date.getMonth() + 1;
-    let y = date.getFullYear();
-    return "" + y + "/" + (m <= 9 ? "0" + m : m) + "/" + (d <= 9 ? "0" + d : d);
-  };
-  let oldDateFormat = dateToYMD(oldDate);
-  let earlyDateFormat = dateToYMD(newDate);
-  console.log(oldDate);
-  console.log(oldDateFormat);
-  let startDateRange = (startDate, endDate) => {
-    let start = startDate.split("/");
-    let end = endDate.split("/");
-    let startYear = parseInt(start[0]);
-    let endYear = parseInt(end[0]);
-    let dates = [];
+	console.log("new", newDate);
 
-    for (let i = startYear; i <= endYear; i++) {
-      let endMonth = i != endYear ? 11 : parseInt(end[1]) - 1;
-      let startMon = i === startYear ? parseInt(start[1]) - 1 : 0;
-      for (let j = startMon; j <= endMonth; j = j > 12 ? j % 12 || 11 : j + 1) {
-        let month = j + 1;
-        let displayMonth = month < 10 ? "0" + month : month;
-        dates.push([i, displayMonth, "01"].join("/"));
-      }
-    }
-    return dates;
-  };
-  let newDateFormat2 = startDateRange(earlyDateFormat, oldDateFormat);
+	// convert (april 28 2021) format to (2021/ 04/ 28)
+	let dateToYMD = (date) => {
+		let d = date.getDate();
+		let m = date.getMonth() + 1;
+		let y = date.getFullYear();
+		return "" + y + "/" + (m <= 9 ? "0" + m : m) + "/" + (d <= 9 ? "0" + d : d);
+	};
+	let oldDateFormat = dateToYMD(oldDate);
+	let earlyDateFormat = dateToYMD(newDate);
+	console.log(oldDate);
+	console.log(oldDateFormat);
+	let startDateRange = (startDate, endDate) => {
+		let start = startDate.split("/");
+		let end = endDate.split("/");
+		let startYear = parseInt(start[0]);
+		let endYear = parseInt(end[0]);
+		let dates = [];
 
-  let lastDate = (date) => {
-    let arr = [];
+		for (let i = startYear; i <= endYear; i++) {
+			let endMonth = i != endYear ? 11 : parseInt(end[1]) - 1;
+			let startMon = i === startYear ? parseInt(start[1]) - 1 : 0;
+			for (let j = startMon; j <= endMonth; j = j > 12 ? j % 12 || 11 : j + 1) {
+				let month = j + 1;
+				let displayMonth = month < 10 ? "0" + month : month;
+				dates.push([i, displayMonth, "01"].join("/"));
+			}
+		}
+		return dates;
+	};
+	let newDateFormat2 = startDateRange(earlyDateFormat, oldDateFormat);
 
-    for (let i = 0; i < date.length; i++) {
-      let check = moment(date[i], "YYYY/MM/DD")
-        .endOf("month")
-        .format("YYYY/MM/DD");
-      arr.push(check);
-    }
-    return arr;
-  };
-  let oldDateFormat2 = lastDate(newDateFormat2);
+	let lastDate = (date) => {
+		let arr = [];
 
-  let eachMonth = (startDate, endDate) => {
-    var fromDate = moment(startDate, "YYYY/MM/DD");
-    var toDate = moment(endDate, "YYYY/MM/DD");
-    var monthData = [];
+		for (let i = 0; i < date.length; i++) {
+			let check = moment(date[i], "YYYY/MM/DD")
+				.endOf("month")
+				.format("YYYY/MM/DD");
+			arr.push(check);
+		}
+		return arr;
+	};
+	let oldDateFormat2 = lastDate(newDateFormat2);
 
-    while (toDate > fromDate || fromDate.format("M") === toDate.format("M")) {
-      monthData.push(fromDate.format("MMMM"));
-      fromDate.add(1, "month");
-    }
-    return monthData;
-  };
+	let eachMonth = (startDate, endDate) => {
+		var fromDate = moment(startDate, "YYYY/MM/DD");
+		var toDate = moment(endDate, "YYYY/MM/DD");
+		var monthData = [];
 
-  let monthName = eachMonth(earlyDateFormat, oldDateFormat);
-  console.log(monthName);
+		while (toDate > fromDate || fromDate.format("M") === toDate.format("M")) {
+			monthData.push(fromDate.format("MMMM"));
+			fromDate.add(1, "month");
+		}
+		return monthData;
+	};
 
-  board.cardObjects.map((d) => {
-    testData.tasks.task.push({
-      start: d.date ? d.date.startDate.slice(0, 10).replaceAll("-", "/") : "",
-      end: d.date ? d.date.endDate : "",
-    });
-    testData.processes.process.push({ label: d.title });
-  });
+	let monthName = eachMonth(earlyDateFormat, oldDateFormat);
+	console.log(monthName);
 
-  for (let i = 0; i < newDateFormat2.length; i++) {
-    testData.categories[0].category.push({
-      start: newDateFormat2[i],
-      end: oldDateFormat2[i],
-      label: monthName[i],
-    });
-  }
+	board.cardObjects.map((d) => {
+		testData.tasks.task.push({
+			start: d.date ? d.date.startDate.slice(0, 10).replaceAll("-", "/") : "",
+			end: d.date ? d.date.endDate : "",
+		});
+		testData.processes.process.push({ label: d.title });
+	});
 
-  let start = new Date(earlyDateFormat);
-  let end = new Date(oldDateFormat);
-  var sDate;
-  var eDate;
-  var dateArr = [];
-  var startDateArr = [];
-  var endDateArr = [];
+	for (let i = 0; i < newDateFormat2.length; i++) {
+		testData.categories[0].category.push({
+			start: newDateFormat2[i],
+			end: oldDateFormat2[i],
+			label: monthName[i],
+		});
+	}
 
-  while (start <= end) {
-    if (start.getDay() == 1 || (startDateArr.length == 0 && !sDate)) {
-      sDate = new Date(start.getTime());
-    }
+	let start = new Date(earlyDateFormat);
+	let end = new Date(oldDateFormat);
+	var sDate;
+	var eDate;
+	var dateArr = [];
+	var startDateArr = [];
+	var endDateArr = [];
 
-    if ((sDate && start.getDay() == 0) || start.getTime() == end.getTime()) {
-      eDate = new Date(start.getTime());
-    }
+	while (start <= end) {
+		if (start.getDay() == 1 || (startDateArr.length == 0 && !sDate)) {
+			sDate = new Date(start.getTime());
+		}
 
-    if (eDate && eDate) {
-      let sDate2 = dateToYMD(sDate);
-      let eDate2 = dateToYMD(eDate);
+		if ((sDate && start.getDay() == 0) || start.getTime() == end.getTime()) {
+			eDate = new Date(start.getTime());
+		}
 
-      startDateArr.push(sDate2);
-      endDateArr.push(eDate2);
-      sDate = undefined;
-      eDate = undefined;
-      sDate2 = undefined;
-      eDate2 = undefined;
-    }
+		if (eDate && eDate) {
+			let sDate2 = dateToYMD(sDate);
+			let eDate2 = dateToYMD(eDate);
 
-    start.setDate(start.getDate() + 1);
-  }
+			startDateArr.push(sDate2);
+			endDateArr.push(eDate2);
+			sDate = undefined;
+			eDate = undefined;
+			sDate2 = undefined;
+			eDate2 = undefined;
+		}
 
-  console.log(startDateArr);
-  console.log(endDateArr);
+		start.setDate(start.getDate() + 1);
+	}
 
-  for (let i = 0; i < startDateArr.length; i++) {
-    testData.categories[1].category.push({
-      start: startDateArr[i],
-      end: endDateArr[i],
-    });
-  }
-  console.log(testData);
-  return (
-    <Container>
-      <Navbar />
+	console.log(startDateArr);
+	console.log(endDateArr);
 
-      <ReactFC
-        type="gantt"
-        width="1500"
-        height="1000"
-        dataFormat="JSON"
-        dataSource={testData}
-      />
-      <SelectShowChart />
-    </Container>
-  );
+	for (let i = 0; i < startDateArr.length; i++) {
+		testData.categories[1].category.push({
+			start: startDateArr[i],
+			end: endDateArr[i],
+		});
+	}
+	console.log(testData);
+	return (
+		<Container>
+			<Navbar />
+
+			<ReactFC
+				type="gantt"
+				width="1500"
+				height="1000"
+				dataFormat="JSON"
+				dataSource={testData}
+			/>
+			<SelectShowChart />
+		</Container>
+	);
 };
 
 export default GanttChart;
